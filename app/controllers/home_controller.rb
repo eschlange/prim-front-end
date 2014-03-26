adda5e155ed52b633cb15109583305413efb9521
Eric Schlange <eric.schlange@northwestern.edu>
2014-03-26 11:56:19 -0500

    Added new files for bootstrap config.

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,11 @@
+# Author::  Eric Schlange (mailto:eric.schlange at northwestern.edu)
+# License:: GPLv2
+
+# Controller used for the home splash.
+
+class HomeController < ApplicationController
+  layout "application"
+
+  def index
+  end
+end
