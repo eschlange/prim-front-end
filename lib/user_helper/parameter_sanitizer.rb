3506cd58395b07090fb09af830f2e52170e13ecf
Eric Schlange <eric.schlange@northwestern.edu>
2014-04-09 09:50:29 -0500

    Removed unnecessary auto-generated spec files and fixed several code standard issues.

--- lib/user_helper/parameter_sanitizer.rb
+++ lib/user_helper/parameter_sanitizer.rb
@@ -1,13 +1,30 @@
 module UserHelper
+  # Helper used to permit several strong parameters that were
+  # added to the default devise user model.
+  # @author Eric Schlange <eric.schlange@northwestern.edu>
   class ParameterSanitizer < Devise::ParameterSanitizer
-
     private
+
     def account_update
-      default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :role_id, :future_contact)
-    end
-    def sign_up
-      default_params.permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :role_id, :future_contact)
+      default_params.permit(:first_name,
+                            :last_name,
+                            :email,
+                            :password,
+                            :password_confirmation,
+                            :current_password,
+                            :role_id,
+                            :future_contact)
     end
 
+    def sign_up
+      default_params.permit(:first_name,
+                            :last_name,
+                            :email,
+                            :password,
+                            :password_confirmation,
+                            :current_password,
+                            :role_id,
+                            :future_contact)
+    end
   end
 end
