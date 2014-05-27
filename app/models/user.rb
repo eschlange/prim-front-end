265bb66e825f4f418122aa0f5067886e76381f0e
Eric Schlange <eric.schlange@northwestern.edu>
2014-05-27 14:54:13 -0500

    Fixed styling issues with the email confirmation page and the registration form.

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,6 +47,6 @@ class User < ActiveRecord::Base
   private
 
   def user_params
-    params.require(:user).permit(:first_name, :last_name, :future_contact)
+    params.require(:user).permit(:first_name, :last_name, :phone, :email, :future_contact)
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140402171738) do
+ActiveRecord::Schema.define(version: 20140527185438) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -119,6 +119,7 @@ ActiveRecord::Schema.define(version: 20140402171738) do
     t.string   "last_name"
     t.boolean  "future_contact",         default: true
     t.integer  "role_id"
+    t.string   "phone"
   end
 
   add_index "users", ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true, using: :btree
