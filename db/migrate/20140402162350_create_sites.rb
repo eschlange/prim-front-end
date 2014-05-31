20c60ae74dea7a412cf93e58326a811faaae5188
eschlange <eric.schlange@gmail.com>
2014-05-31 16:56:53 -0500

    Fixed an issue with seed data for interventions that caused a db:reset to fail.

--- db/migrate/20140402162350_create_sites.rb
+++ db/migrate/20140402162350_create_sites.rb
@@ -2,7 +2,7 @@ class CreateSites < ActiveRecord::Migration
   def change
     create_table :sites do |t|
       t.string :name
-      t.text :description
+      t.text :content
 
       t.timestamps
     end
