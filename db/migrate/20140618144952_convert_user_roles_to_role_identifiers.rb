1b1dd989470b58e9548935d81a3e752cfa464ebc
eschlange <eric.schlange@gmail.com>
2014-06-18 10:23:29 -0500

    Actually saving the role modifications in the new migration.

--- db/migrate/20140618144952_convert_user_roles_to_role_identifiers.rb
+++ db/migrate/20140618144952_convert_user_roles_to_role_identifiers.rb
@@ -1,11 +1,12 @@
 class ConvertUserRolesToRoleIdentifiers < ActiveRecord::Migration
   def change
     User.all.each do |user|
-      if  'eric.schlange@northwestern.edu' == user.email
+      if 'eric.schlange@northwestern.edu' == user.email
         user.role_identifier = 1
-      elsif
-      's.galvin@northwestern.edu' == user.email
+        user.save
+      elsif 's.galvin@northwestern.edu' == user.email
         user.role_identifier = 2
+        user.save
       end
     end
   end
