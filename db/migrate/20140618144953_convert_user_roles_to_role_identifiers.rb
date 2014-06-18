e8f27193aaa3171d64e1b4ada7afbcffa0b371db
eschlange <eric.schlange@gmail.com>
2014-06-18 11:13:22 -0500

    Fixed a migration for seeding staging data.

---
+++ db/migrate/20140618144953_convert_user_roles_to_role_identifiers.rb
@@ -0,0 +1,10 @@
+class ConvertUserRolesToRoleIdentifiers < ActiveRecord::Migration
+  def change
+    User.all.each do |user|
+      if 's-galvin@northwestern.edu' == user.email
+        user.role_identifier = 2
+        user.save
+      end
+    end
+  end
+end
