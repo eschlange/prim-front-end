c106f71f8944b0b336e381047a813f2a0d666639
Eric Schlange <eric.schlange@northwestern.edu>
2014-09-03 15:20:36 -0500

    Fixed a navigation error when resending confirmation instructions. Fixed a style issue with the screening page.

--- app/controllers/confirmations_controller.rb
+++ app/controllers/confirmations_controller.rb
@@ -22,6 +22,30 @@ class ConfirmationsController < Devise::ConfirmationsController
     end
   end
 
+  def create
+
+    self.resource = send_confirmation_instructions(resource_params)
+    yield resource if block_given?
+
+    if successfully_sent?(resource)
+      respond_with({}, location: after_resending_confirmation_instructions_path_for(resource_name))
+    else
+      respond_with(resource)
+    end
+  end
+
+  def send_confirmation_instructions(attributes={})
+    confirmable = find_by_unconfirmed_email_with_errors(attributes) if resource_class.reconfirmable
+    unless confirmable.try(:persisted?)
+      confirmable = resource_class.find_or_initialize_with_errors(resource_class.confirmation_keys, attributes, :not_found)
+    end
+
+    confirmable.site_id = params[:site_id]
+
+    confirmable.resend_confirmation_instructions if confirmable.persisted?
+    confirmable
+  end
+
   protected
 
   def sign_in_and_redirect(resource_or_scope, *args)
