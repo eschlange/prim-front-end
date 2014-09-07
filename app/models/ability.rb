5ad51ebce7d8b20fea6d69e1413237378e48d6a6
eschlange <eric.schlange@gmail.com>
2014-09-07 11:53:22 -0500

    Removed ability for guests to see admin dashboard..

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -7,17 +7,19 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     user ||= User.new # guest user (not logged in)
-    can :access, :rails_admin   # grant access to rails_admin
-    can :dashboard              # grant access to the dashboard
 
     if user.at_least_a_super_user?
       can :manage, :all
+      can :access, :rails_admin   # grant access to rails_admin
+      can :dashboard              # grant access to the dashboard
     elsif user.at_least_a_admin?
       can :manage, :all
+      can :access, :rails_admin   # grant access to rails_admin
+      can :dashboard              # grant access to the dashboard
     elsif user.at_least_a_content_manager?
       can :manage, :all
-    elsif user.at_least_a_participant?
-      can :read, :all
+      can :access, :rails_admin   # grant access to rails_admin
+      can :dashboard              # grant access to the dashboard
     end
 
     #
